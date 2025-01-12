a76a082e4fbb4f2ae4822082ae724a2d105adbb3
Marco Zerbinati <zerbinati.marco@gmail.com>
2025-01-12 10:30:32 +0100

    JudaS ++ 4.0

--- src/misc.cpp
+++ src/misc.cpp
@@ -42,7 +42,7 @@ namespace Judas {
 namespace {
 
 // Version number or dev.
-constexpr std::string_view version = "++ 3.0";
+constexpr std::string_view version = "++ 4.0";
 
 // Our fancy logging facility. The trick here is to replace cin.rdbuf() and
 // cout.rdbuf() with two Tie objects that tie cin and cout to a file stream. We
